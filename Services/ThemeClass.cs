28a3137727309de762771bd6ef60f7a778ce2341
Lacro59 <lacro59@gmail.com>
2020-09-07 10:19:09 +0200

    Resolve a problem on exit

--- Services/ThemeClass.cs
+++ Services/ThemeClass.cs
@@ -174,181 +174,188 @@ namespace ThemeModifier.Services
 
         public static void SetThemeSettings(ThemeModifierSettings settings)
         {
-            IntegrationUI ui = new IntegrationUI();
-            List<ResourcesList> resourcesLists = new List<ResourcesList>();
+            try
+            {
+                IntegrationUI ui = new IntegrationUI();
+                List<ResourcesList> resourcesLists = new List<ResourcesList>();
 
-            string[] TwoColors = null;
-            if (!settings.ControlBackgroundBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                string[] TwoColors = null;
+                if (!settings.ControlBackgroundBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "ControlBackgroundBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.ControlBackgroundBrush_Edit))
-                });
-            }
-            if (!settings.TextBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "ControlBackgroundBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.ControlBackgroundBrush_Edit))
+                    });
+                }
+                if (!settings.TextBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "TextBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TextBrush_Edit))
-                });
-            }
-            if (!settings.TextBrushDarker_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "TextBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TextBrush_Edit))
+                    });
+                }
+                if (!settings.TextBrushDarker_Edit.IsNullOrEmpty())
                 {
-                    Key = "TextBrushDarker",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TextBrushDarker_Edit))
-                });
-            }
-            if (!settings.TextBrushDark_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "TextBrushDarker",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TextBrushDarker_Edit))
+                    });
+                }
+                if (!settings.TextBrushDark_Edit.IsNullOrEmpty())
                 {
-                    Key = "TextBrushDark",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TextBrushDark_Edit))
-                });
-            }
-            if (!settings.NormalBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "TextBrushDark",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TextBrushDark_Edit))
+                    });
+                }
+                if (!settings.NormalBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "NormalBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NormalBrush_Edit))
-                });
-            }
-            if (!settings.NormalBrushDark_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "NormalBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NormalBrush_Edit))
+                    });
+                }
+                if (!settings.NormalBrushDark_Edit.IsNullOrEmpty())
                 {
-                    Key = "NormalBrushDark",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NormalBrushDark_Edit))
-                });
-            }
-            if (!settings.NormalBorderBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "NormalBrushDark",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NormalBrushDark_Edit))
+                    });
+                }
+                if (!settings.NormalBorderBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "NormalBorderBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NormalBorderBrush_Edit))
-                });
-            }
-            if (!settings.HoverBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "NormalBorderBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NormalBorderBrush_Edit))
+                    });
+                }
+                if (!settings.HoverBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "HoverBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.HoverBrush_Edit))
-                });
-            }
-            if (!settings.GlyphBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "HoverBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.HoverBrush_Edit))
+                    });
+                }
+                if (!settings.GlyphBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "GlyphBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.GlyphBrush_Edit))
-                });
-            }
-            if (!settings.HighlightGlyphBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "GlyphBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.GlyphBrush_Edit))
+                    });
+                }
+                if (!settings.HighlightGlyphBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "HighlightGlyphBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.HighlightGlyphBrush_Edit))
-                });
-            }
-            if (!settings.PopupBorderBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "HighlightGlyphBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.HighlightGlyphBrush_Edit))
+                    });
+                }
+                if (!settings.PopupBorderBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "PopupBorderBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PopupBorderBrush_Edit))
-                });
-            }
-            if (!settings.TooltipBackgroundBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "PopupBorderBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PopupBorderBrush_Edit))
+                    });
+                }
+                if (!settings.TooltipBackgroundBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "TooltipBackgroundBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TooltipBackgroundBrush_Edit))
-                });
-            }
-            if (!settings.ButtonBackgroundBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "TooltipBackgroundBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.TooltipBackgroundBrush_Edit))
+                    });
+                }
+                if (!settings.ButtonBackgroundBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "ButtonBackgroundBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.ButtonBackgroundBrush_Edit))
-                });
-            }
-            if (!settings.GridItemBackgroundBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "ButtonBackgroundBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.ButtonBackgroundBrush_Edit))
+                    });
+                }
+                if (!settings.GridItemBackgroundBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "GridItemBackgroundBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.GridItemBackgroundBrush_Edit))
-                });
-            }
-            if (!settings.PanelSeparatorBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "GridItemBackgroundBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.GridItemBackgroundBrush_Edit))
+                    });
+                }
+                if (!settings.PanelSeparatorBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "PanelSeparatorBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PanelSeparatorBrush_Edit))
-                });
-            }
-            if (!settings.PopupBackgroundBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "PanelSeparatorBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PanelSeparatorBrush_Edit))
+                    });
+                }
+                if (!settings.PopupBackgroundBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "PopupBackgroundBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PopupBackgroundBrush_Edit))
-                });
-            }
-            if (!settings.PositiveRatingBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "PopupBackgroundBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PopupBackgroundBrush_Edit))
+                    });
+                }
+                if (!settings.PositiveRatingBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "PositiveRatingBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PositiveRatingBrush_Edit))
-                });
-            }
-            if (!settings.NegativeRatingBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "PositiveRatingBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.PositiveRatingBrush_Edit))
+                    });
+                }
+                if (!settings.NegativeRatingBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "NegativeRatingBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NegativeRatingBrush_Edit))
-                });
-            }
-            if (!settings.MixedRatingBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "NegativeRatingBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.NegativeRatingBrush_Edit))
+                    });
+                }
+                if (!settings.MixedRatingBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "MixedRatingBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.MixedRatingBrush_Edit))
-                });
-            }
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "MixedRatingBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.MixedRatingBrush_Edit))
+                    });
+                }
 
-            if (!settings.ExpanderBackgroundBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                if (!settings.ExpanderBackgroundBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "ExpanderBackgroundBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.ExpanderBackgroundBrush_Edit))
-                });
-            }
-            if (!settings.WindowBackgourndBrush_Edit.IsNullOrEmpty())
-            {
-                resourcesLists.Add(new ResourcesList
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "ExpanderBackgroundBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.ExpanderBackgroundBrush_Edit))
+                    });
+                }
+                if (!settings.WindowBackgourndBrush_Edit.IsNullOrEmpty())
                 {
-                    Key = "WindowBackgourndBrush",
-                    Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.WindowBackgourndBrush_Edit))
-                });
-            }
+                    resourcesLists.Add(new ResourcesList
+                    {
+                        Key = "WindowBackgourndBrush",
+                        Value = new SolidColorBrush((Color)ColorConverter.ConvertFromString(settings.WindowBackgourndBrush_Edit))
+                    });
+                }
 
-            ui.AddResources(resourcesLists);
+                ui.AddResources(resourcesLists);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
